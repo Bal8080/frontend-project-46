c9ddc267fc2f87a9b91bb891ca4c6cf53f09668c
Bal8080 <bb8080t1000@gmail.com>
2025-01-13 11:13:43 +0300

    add part 4

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js'
+import compareData from './compareData.js';
+import format from './formatters/format.js';
 
 const getAbsolutPath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getAbsolutPath(filepath), 'utf-8');
 const getFormat = (filename) => filename.split('.')[1];
 
-const genDiff = (filepath1, filepath2) => {
+const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
     console.log('File 1:');
-    const f1 = parse(readFile(filepath1), getFormat(filepath1));
-    console.log(f1);
+    const parsed1 = parse(readFile(filepath1), getFormat(filepath1));
+    console.log(parsed1);
     console.log();
     console.log('File 2:');
-    const f2 = parse(readFile(filepath2), getFormat(filepath2));
-    console.log(f2);
+    const parsed2 = parse(readFile(filepath2), getFormat(filepath2));
+    console.log(parsed2);
+    const data = compareData(parsed1, parsed2);
+
+    return format(data, formatName);
 }
 
 export default genDiff;
