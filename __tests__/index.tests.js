97fb5288501d6ab5d2459e52771a726bf36d62bf
Bal8080 <bb8080t1000@gmail.com>
2025-01-15 12:32:23 +0300

    add tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -2,13 +2,24 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
-const getAbsolutPath = (filepath) => path.resolve(process.cwd(), filepath);
-const file1Path = getAbsolutPath('__fixtures__/file1.json');
-const file2Path = getAbsolutPath('__fixtures__/file2.json');
-const resPath = getAbsolutPath('__fixtures__/result.txt');
-console.log(genDiff(file1Path, file2Path), 1111111111);
-console.log(fs.readFileSync(resPath).toString(), 22222222222);
+const fileExt = [".json", ".yml"];
 
-test('gendiff', () => {
-  expect(genDiff(file1Path, file2Path)).toEqual(fs.readFileSync(resPath, 'utf-8'));
+const resultStylish = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result_stylish.txt"),
+	"utf-8"
+);
+const resultPlain = fs.readFileSync(
+	path.resolve(process.cwd(), "__fixtures__/result_plain.txt"),
+	"utf-8"
+);
+
+test.each(fileExt)("testing different file options", (extension) => {
+	const fileBefore = `__fixtures__/file1${extension}`;
+	const fileAfter = `__fixtures__/file2${extension}`;
+	const actual1 = genDiff(fileBefore, fileAfter, "stylish");
+	expect(actual1).toEqual(resultStylish);
+	const actual2 = genDiff(fileBefore, fileAfter, "plain");
+	expect(actual2).toEqual(resultPlain);
+	const actual4 = genDiff(fileBefore, fileAfter);
+	expect(actual4).toEqual(resultStylish);
 });
